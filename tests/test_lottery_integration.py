8dbee9000a828129e6443fac31f84a91b1e6a0ea
francisco <fr.betancourt@live.com>
2022-01-06 18:03:42 -0300

    First commit

---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,28 @@
+from brownie import network
+import pytest
+import time
+from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+)
+
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter(
+        {"from": account, "value": lottery.getEntranceFee() + 10000000000000000}
+    )
+    lottery.enter(
+        {"from": account, "value": lottery.getEntranceFee() + 10000000000000000}
+    )
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(180)
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
